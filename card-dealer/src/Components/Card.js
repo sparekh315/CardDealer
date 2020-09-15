22cc5f06ac7ea94fe756a534bab4931b3b1b48a6
sparekh315 <saniaparekh315@gmail.com>
2020-09-14 22:43:26 -0500

    card dealer project

---
+++ card-dealer/src/Components/Card.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import './Card.css'
+
+
+
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        let angle = Math.random() * 90 - 45;
+        let xPos = Math.random() * 40 - 20;
+        let yPos = Math.random() * 40 -20;
+        this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    }
+    render() {
+        return(
+            <img 
+                className='Card'
+                style={{ transform: this._transform}}
+                src={this.props.image} 
+                alt={this.props.name}>
+            </img>
+        );
+    };
+};
+
+
+export default Card;
